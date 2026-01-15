f6bb8df45d0271f81d242953dab5fbd1e4c2533b
abdullah-afzal <abdullah-afzal@users.noreply.github.com>
2026-01-15 21:25:36 +0000

    deploy: c85d8f4bf12597df1247eb6e107cc0e892e04ad3

---
+++ city-theme.js
@@ -0,0 +1,18 @@
+async function getContributions(token, username) {
+  const query = `
+    query($userName:String!) {
+      user(login: $userName) {
+        contributionsCollection {
+          contributionCalendar {
+            weeks {
+              contributionDays {
+                contributionCount
+                date
+              }
+            }
+          }
+        }
+      }
+    }
+  `;
+}
